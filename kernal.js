e759ba7b925f09b59be76f645cf5d30244f086b5
Jeremiah <46662314+UCYT5040@users.noreply.github.com>
2021-09-24 20:52:21 -0500

    Update kernal.js

--- kernal.js
+++ kernal.js
@@ -1,1 +1,34 @@
+/*
+KERNAL JS
+-----
+*/
+// begin kernal class
+let Kernal = class {
+    // Init
 
+    constructor() {
+        this.version = "1.0.0";
+        this.views = []
+    }
+
+// end kernal class
+}
+let k = new Kernal()
+k.View = class {
+    constructor(html) {
+        this.html = html
+    }
+}
+k.Component = class {
+    constructor(view) {
+        this.view = view;
+    }
+    v(args) {
+        let tempView = this.view;
+        Object.keys(args).forEach(function (item, index) {
+            tempView.html = tempView.html.replaceAll("{"+item+"}", args[item]);
+        });
+        tempView = tempView.replaceAll;("kernal:open_curly", "{").replaceAll("kernal:closing_curly", "}");
+        return tempView;
+    }
+}
